b45141ad307ce2d89a3fec7a987bed21f0ddd189
kioria95 <kenkioria@gmail.com>
2020-10-16 13:45:52 +0300

    Added functionality to retrieve messages from Firebase

--- src/Chat.js
+++ src/Chat.js
@@ -3,26 +3,62 @@ import React, { useState, useEffect } from "react";
 import "./Chat.css";
 import db from "./firebase";
 import MicNoneIcon from "@material-ui/icons/MicNone";
+import Message from "./Message";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
+import { selectChatName, selectChatId } from "./features/chatSlice";
+
+import firebase from "firebase";
 
 function Chat() {
   const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const user = useSelector(selectUser);
+  const chatName = useSelector(selectChatName);
+  const chatId = useSelector(selectChatId);
 
   const sendMessage = (e) => {
     e.preventDefault();
+    db.collection("chats").doc(chatId).collection("messages").add({
+      message: input,
+      uid: user.uid,
+      photo: user.photo,
+      email: user.email,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
     setInput("");
   };
+
+  useEffect(() => {
+    if (chatId) {
+      db.collection("chats")
+        .doc(chatId)
+        .collection("messages")
+        .orderBy("timestamp", "desc")
+        .onSnapshot((snapshot) =>
+          setMessages(
+            snapshot.docs.map((doc) => ({
+              id: doc.id,
+              data: doc.data(),
+            }))
+          )
+        );
+    }
+  }, [chatId]);
 
   return (
     <div className="chat">
       <div className="chat__header">
         <h4>
-          To: <span className="chat__name">Chatting</span>
+          To: <span className="chat__name">{chatName}</span>
         </h4>
         <strong>Details</strong>
       </div>
 
       <div className="chat__messages">
-          
+        {messages.map(({ id, data }) => (
+          <Message key={id} contents={data} />
+        ))}
       </div>
 
       <div className="chat__input">
