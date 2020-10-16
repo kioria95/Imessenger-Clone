3bab207246dbed64780817b62e3d582eb27b7afb
kioria95 <kenkioria@gmail.com>
2020-10-16 12:45:06 +0300

    Added Login functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Imessage from "./Imessage";
+import { selectUser,login, logout } from "./features/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import Login from "./Login";
+import { auth } from "./firebase";
 
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+       
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          email: authUser.email,
+          displayName: authUser.displayName,
+        }))
+      } else {
+      }
+    });
+  }, []);
   return (
     <div className="app">
-      <Imessage />
+      {user ? (
+        <>
+          <Imessage />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
