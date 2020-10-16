5967d7c521385a18610d437c2b9bbcbe950d3d4e
kioria95 <kenkioria@gmail.com>
2020-10-16 11:47:02 +0300

    Added code to Chat.js and Chat.css

--- src/Chat.js
+++ src/Chat.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import './Chat.css'
+import React, { useState, useEffect } from "react";
+import "./Chat.css";
 
 function Chat() {
-    return (
-        <div className="chat">
-            
-        </div>
-    )
+  const [input, setInput] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    setInput("");
+  };
+  
+  return (
+    <div className="chat">
+      <div className="chat__header">
+        <h4>
+          To: <span className="chat__name">Chatting</span>
+        </h4>
+        <strong>Details</strong>
+      </div>
+
+      <div className="chat__input">
+        <form>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+            placeholder="iMessage"
+          />
+          <button onClick={sendMessage}>Send Message</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Chat
+export default Chat;
